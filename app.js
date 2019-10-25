7e2ec513184757c4923f94a134d0712bd1e64f80
hussamAdil <hussam0683@gmail.com>
2019-10-25 16:53:34 +0200

    set style for attack

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
         playerHealth:100,
         monsterHealth:100,
         isGameRunning:false,
+        turns:[]
     },
     methods:{
         startGame:function()
@@ -11,28 +12,65 @@ new Vue({
             this.playerHealth  = 100
             this.monsterHealth = 100
             this.isGameRunning = true
+            this.turns = []
         },
         attack:function()
         {
-            this.monsterHealth -= this.calculateDamage(3,10);
+            let damage = this.calculateDamage(3,10)
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer:true,
+                text:'Play hits monster for ' + damage
+            })
             if(this.checkWin())
             {
                 return;
             }
-            this.playerHealth -= this.calculateDamage(5,12);
-            this.checkWin();
+         this.monsterAttack();
         },
         specialAttack:function()
         {
-
+            let damage = this.calculateDamage(10,20)
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer:true,
+                text:'Play hits monster for ' + damage
+            })
+            if(this.checkWin())
+            {
+                return;
+            }
+         this.monsterAttack();
         },
         heal:function()
         {
-
+            if(this.playerHealth <= 90)
+            {
+                this.playerHealth +=10
+                
+                this.turns.unshift({
+                    isPlayer:true,
+                    text:'Play heals 10'
+                })
+            }else
+            {
+                this.playerHealth = 100
+            }
         },
         giveUp:function()
         {
-
+            this.isGameRunning = false
+            this.turns = []
+        },
+        monsterAttack:function()
+        {
+            let damage =  this.calculateDamage(5,12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer:false,
+                text:'monster hits player for ' + damage
+            })
+            this.checkWin();
         },
         calculateDamage:function(min,max)
         {
